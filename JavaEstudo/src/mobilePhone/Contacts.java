72b6dbfa2ec0bab9f70792c142a18c0df1d1e33b
Thiago Novaes <thiagognovaes@gmail.com>
2020-03-08 10:20:25 -0300

    Repositorio de estudo, criando o projeto e adicionando o começo do package de Contatcs utilizando ArrayList

---
+++ JavaEstudo/src/mobilePhone/Contacts.java
@@ -0,0 +1,37 @@
+package mobilePhone;
+
+import java.util.ArrayList;
+
+public class Contacts {
+
+	private String name;
+	private String phoneNumber;
+	
+	//Constructor
+	public Contacts(String name, String phoneNumber) {
+		this.name=name;
+		this.phoneNumber = phoneNumber;
+	}
+	//Getters And Setters
+	public String getName() {
+		return name;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+
+
+	
+	
+	
+	
+}
